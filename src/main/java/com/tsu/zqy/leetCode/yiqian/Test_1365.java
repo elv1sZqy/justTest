ee1b8fdfa5405cdcfe3eada2c9058795d3129fba
qiyun.zhu@itrigger.com <nihaoa>
2020-10-26 15:21:19 +0800

    [公司电脑] leetcode代码其中一题加一种实现

--- src/main/java/com/tsu/zqy/leetCode/yiqian/Test_1365.java
+++ src/main/java/com/tsu/zqy/leetCode/yiqian/Test_1365.java
@@ -1,5 +1,6 @@
 package com.tsu.zqy.leetCode.yiqian;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,9 +10,31 @@ import java.util.Map;
  * @description :
  */
 public class Test_1365 {
-    // 暴力法
-    // 加一个缓存cache
+
+    // 先做排序    不算排序...  O(n)
     public int[] smallerNumbersThanCurrent(int[] nums) {
+        HashMap<Integer, Integer> cache = new HashMap<>();
+        int[] copy = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(copy);
+        for (int i = 0; i < copy.length; i++) {
+            int num = copy[i];
+            if (0 == i) {
+                cache.put(num, 0);
+                continue;
+            }
+            if (cache.containsKey(num)) continue;
+            cache.put(num, i);
+        }
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = cache.get(nums[i]);
+        }
+        return result;
+    }
+
+    // 暴力法
+    // 加一个缓存cache   O(n^2)
+    public int[] smallerNumbersThanCurrent1(int[] nums) {
         Map<Integer, Integer> cache = new HashMap<>();
         int[] result = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {

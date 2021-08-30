9be77eff3e817161e9f18cef63915a555430e9ea
qiyun.zhu <qiyun.zhu@itrigger.com>
2021-08-30 10:39:22 +0800

    [公司电脑] leetcode练习

--- src/main/java/com/tsu/zqy/leetCode/steptwo/Test_206.java
+++ src/main/java/com/tsu/zqy/leetCode/steptwo/Test_206.java
@@ -4,7 +4,10 @@ import com.google.common.collect.Lists;
 import com.tsu.zqy.leetCode.wrapper.ListNode;
 import com.tsu.zqy.leetCode.wrapper.ListNodeWrapper;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  * @author zhuQiYun
@@ -22,10 +25,48 @@ public class Test_206 {
     public static void main(String[] args) {
         String l = "(1,2,3,4,6)";
         ListNode listNode = ListNodeWrapper.stringToListNode(l);
-        new Test_206().reverseList(listNode);
+        ListNode listNode1 = new Test_206().reverseList(listNode);
+        ListNodeWrapper.prettyPrintLinkedList(listNode1);
     }
 
-    public ListNode reverseList(ListNode head) {
+    private ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode nextNode = head.next;
+        head.next = null;
+        while (nextNode != null) {
+            ListNode next = nextNode.next;
+            nextNode.next = head;
+            if (next == null) {
+                return nextNode;
+            }
+            head = nextNode;
+            nextNode = next;
+        }
+        return head;
+    }
+
+    private ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        Queue<ListNode> queue = new LinkedList<>();
+        while (head != null) {
+            queue.add(head);
+            head = head.next;
+        }
+        head = queue.remove();
+        head.next = null;
+        while (!queue.isEmpty()) {
+            ListNode remove = queue.remove();
+            remove.next = head;
+            head = remove;
+        }
+        return head;
+    }
+
+    public ListNode reverseList1(ListNode head) {
         if (head == null || head.next == null){
             return head;
         }
